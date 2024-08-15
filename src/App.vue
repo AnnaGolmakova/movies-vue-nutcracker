29e8ef6bddd215c2cb3583426ecb315af6c596fb
Anna Golmakova <ann.golmakova@gmail.com>
2024-08-15 20:11:14 +0100

    Created list component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import CardList from './components/CardList.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <CardList>
 </template>
 
 <style scoped>
